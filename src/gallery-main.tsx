fab3f92d6e9610df8823a9f84bc882417c771a04
Yuetong Li <liyt2023@shanghaitech.edu.cn>
2026-08-26 20:38:39 +0800

    Separate gallery into its own page

--- src/gallery-main.tsx
+++ src/gallery-main.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Portfolio from "./Portfolio";
+import Gallery from "./Gallery";
 import "./styles.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Portfolio />
+    <Gallery />
   </React.StrictMode>,
 );
